94d7fce2bceace2fc02ce1ca54c215881d270fe7
Suzuki <oasuzuki@dphe.state.co.us>
2025-11-25 16:53:06 -0700

    Initial commit of the existing R project

---
+++ 02_Scripts/Permits_Dewatering_Data_Monitoring_Reports_GW_WW.R
@@ -0,0 +1,452 @@
+# PERMITS - DATA MONITORING REPORTS.
+
+
+# 1: Importing the data ####
+
+library(readxl)
+library(dplyr)
+
+
+# Importing data
+DMR_RawData <- read_excel(
+  "X:\\Shared drives\\_CDPHE TEEO Data\\_Enviro\\PFAS\\PFAS Concentration Map May 2024 Update\\PFASMap 2024_RProject\\01_Raw_Data\\DMR Data Report PFAS 20240701.xlsx"
+)
+
+
+
+# 2: Misc Data Cleaning ####
+
+
+# Replace all spaces in names of variables with underscores
+names(DMR_RawData) <- gsub("\\s", "_", names((DMR_RawData)))
+
+# Change ug/L units to ng/L because the ug/L entries were a typo
+DMR_RawData <- DMR_RawData %>% mutate(
+  Concentration_Units = case_when(
+    Concentration_Units == "ug/L" ~ "ng/L",
+    TRUE ~ Concentration_Units
+  )
+)
+
+# Create a table to see the types of PFAS analytes included in the DF
+PFAS_Types <- data.frame(table(DMR_RawData$Parameter))
+
+
+# Check for duplicates of the same types of PFAS within NPDES ID for the same sampling date and same PFAS analyte. This creates a dataframe from them called "duplicates"
+DMR_RawData_1 <- DMR_RawData %>%
+  group_by(NPDES_ID, DMR_Received_Date, Parameter) %>%
+  distinct() %>%
+  ungroup()
+
+
+# Delete any observations with NODI codes other than NODI B
+Codes_Delete <- c(
+  "NODI C",
+  "NODI **E**",
+  "NODI 7",
+  "NODI 9" ,
+  "NODI **P**",
+  "NODI **8**",
+  "NODI N",
+  "NODI 2",
+  "NODI 3",
+  "NODI F"
+)
+
+# remove values of concentration 2 that are = "NODI C", "NODI **E**", "NODI 7", "NODI 9" ,"NODI **P**", or "NODI **8**"
+DMR_RawData_2  <- DMR_RawData_1 %>%
+  filter(!Concentration_2 %in% Codes_Delete)
+
+DMR_RawData_3  <- DMR_RawData_2 %>%
+  filter(!Concentration_3 %in% Codes_Delete)
+
+
+# Create a variable with PFAS abbreviations called PFAS_Analyte
+# Add PFOA
+
+DMR_RawData_3 <- DMR_RawData_3 %>%
+  mutate(
+    PFAS_Analyte = case_when(
+      Parameter %in% c("Perfluorooctanesulfonamide") ~ "PFOSA",
+      Parameter %in% c("Perfluoroheptanoic acid") ~ "PFHpA",
+      Parameter %in% c("Perfluorobutanesulfonic acid") ~ "PFBS",
+      Parameter %in% c("Perfluorododecanoic acid") ~ "PFDoA",
+      Parameter %in% c("N-Methylperfluorooctanesulfonamide") ~ "NMeFOSA",
+      Parameter %in% c("Perfluorononanoic acid") ~ "PFNA",
+      Parameter %in% c("Perfluoroundecanoic acid") ~ "PFUnA",
+      Parameter %in% c("N-Ethylperfluorooctanesulfonamide") ~ "NEtFOSA",
+      Parameter %in% c("Perfluorohexanoic acid") ~ "PFHxA",
+      Parameter %in% c("Perfluorotridecanoic acid") ~ "PFTrDA",
+      Parameter %in% c("Perfluorononane sulfonic acid") ~ "PFNS",
+      Parameter %in% c("Perfluorotetradecanoic acid") ~ "PFTeDA",
+      Parameter %in% c("Perfluoropentane sulfonic acid") ~ "PFPeS",
+      Parameter %in% c("Perfluoroheptanesulfonic acid") ~ "PFHpS",
+      Parameter %in% c("4,8-Dioxa-3H-perfluorononanoic acid") ~ "ADONA",
+      Parameter %in% c("Hexafluoropropylene oxide dimer acid") ~ "GenX",
+      Parameter %in% c("Perfluorodecanoic acid") ~ "PFDA",
+      Parameter %in% c("Perfluoropentanoic acid") ~ "PFPeA",
+      Parameter %in% c("Perfluorooctanoic Acid") ~ "PFOA",
+      Parameter %in% c("Perfluorohexanesulfonic acid") ~ "PFHxS",
+      Parameter %in% c("2-[N-ethylperfluorooctanesulfonamido] acetic acid") ~ "NEtFOSAA",
+      Parameter %in% c("Perfluorodecanesulfonic acid") ~ "PFDS",
+      Parameter %in% c("Perfluorododecanesulfonic acid") ~ "PFDoS",
+      Parameter %in% c("Perfluorooctanesulfonic acid") ~ "PFOS",
+      Parameter %in% c("N-methyl perfluorooctanesulfonamidoacetic acid") ~ "NMeFOSAA",
+      Parameter %in% c("Perfluoro-3,6-dioxaheptanoic acid") ~ "NFDHA",
+      Parameter %in% c("Perfluorobutanoic Acid") ~ "PFBA",
+      Parameter %in% c("Perfluoroethoxyethanesulfonic acid") ~ "PFEESA",
+      Parameter %in% c("Potassium 9-chlorohexadecafluoro-3-oxanonane-1-sulfonate") ~ "9Cl-PF3ONS",
+      Parameter %in% c("Perfluoro-4-methoxybutanoic acid") ~ "PFMBA",
+      Parameter %in% c("Perfluoro-2-methoxypropanoic acid") ~ "PFMPA",
+      Parameter %in% c("11-Chloroeicosafluoro-3-oxaundecane-1-sulfonic Acid") ~ "11Cl-PF3OUdS",
+      Parameter %in% c("Potassium 11-chloroeicosafluoro-3-oxaundecane-1-sulfonate") ~ "11Cl-PF3OUdS",
+      Parameter %in% c("4:2 Fluorotelomer sulfonic acid") ~ "4:2FTS",
+      Parameter %in% c("6:2 Fluorotelomer sulfonic acid") ~ "6:2FTS",
+      Parameter %in% c("3-Perfluoropropyl propanoic acid") ~ "3:3FTCA",
+      Parameter %in% c("8:2 Fluorotelomer sulfonic acid") ~ "8:2FTS",
+      Parameter %in% c("N-methylperfluorooctane Sulfonamidoethanol") ~ "NMeFOSE",
+      Parameter %in% c("N-ethylperfluorooctane Sulfonamidoethanol") ~ "NEtFOSE",
+      Parameter %in% c("3-Perfluoroheptyl propanoic acid") ~ "7:3FTCA",
+      Parameter %in% c("2H,2H,3H,3H-Perfluorooctanoic acid") ~ "5:3FTCA",
+      TRUE ~ Parameter
+    )
+  )
+
+
+#Create a variable that has the MDL values for each type of PFAS analyte
+DMR_RawData_4 <- DMR_RawData_3 %>%
+  mutate(
+    MDL = case_when(
+      PFAS_Analyte == "PFOSA" ~ 0.32,
+      PFAS_Analyte == "PFHpA" ~ 0.37,
+      PFAS_Analyte == "PFBS" ~ 0.37,
+      PFAS_Analyte == "PFDoA" ~ 0.40,
+      PFAS_Analyte == "NMeFOSA" ~ 0.43,
+      PFAS_Analyte == "PFNA" ~ 0.45,
+      PFAS_Analyte == "PFUnA" ~ 0.45,
+      PFAS_Analyte == "NEtFOSA" ~ 0.45,
+      PFAS_Analyte == "PFHxA" ~ 0.46,
+      PFAS_Analyte == "PFTrDA" ~ 0.46,
+      PFAS_Analyte == "PFNS" ~ 0.47,
+      PFAS_Analyte == "PFTeDA" ~ 0.49,
+      PFAS_Analyte == "PFPeS" ~ 0.50,
+      PFAS_Analyte == "PFHpS" ~ 0.50,
+      PFAS_Analyte == "ADONA" ~ 0.50,
+      PFAS_Analyte == "GenX" ~ 0.51,
+      PFAS_Analyte == "PFDA" ~ 0.52,
+      PFAS_Analyte == "PFPeA" ~ 0.54,
+      PFAS_Analyte == "PFOA" ~ 0.54,
+      PFAS_Analyte == "PFHxS" ~ 0.54,
+      PFAS_Analyte == "NEtFOSAA" ~ 0.59,
+      PFAS_Analyte == "PFDS" ~ 0.60,
+      PFAS_Analyte == "PFDoS" ~ 0.60,
+      PFAS_Analyte == "PFOS" ~ 0.63,
+      PFAS_Analyte == "NMeFOSAA" ~ 0.68,
+      PFAS_Analyte == "NFDHA" ~ 0.75,
+      PFAS_Analyte == "PFBA" ~ 0.79,
+      PFAS_Analyte == "PFEESA" ~ 1.17,
+      PFAS_Analyte == "9Cl-PF3ONS" ~ 1.38,
+      PFAS_Analyte == "PFMBA" ~ 1.41,
+      PFAS_Analyte == "PFMPA" ~ 1.46,
+      PFAS_Analyte == "11Cl-PF3OUdS" ~ 1.67,
+      PFAS_Analyte == "4:2FTS" ~ 1.69,
+      PFAS_Analyte == "6:2FTS" ~ 2.45,
+      PFAS_Analyte == "3:3FTCA" ~ 2.47,
+      PFAS_Analyte == "8:2FTS" ~ 2.50,
+      PFAS_Analyte == "NMeFOSE" ~ 3.81,
+      PFAS_Analyte == "NEtFOSE" ~ 4.84,
+      PFAS_Analyte == "7:3FTCA" ~ 8.71,
+      PFAS_Analyte == "5:3FTCA" ~ 9.59
+    )
+  )
+
+
+# 3: Substituting non-detects for zero ####
+
+# Replace ND values with 0 and NA values with 0
+
+# Concentration_3
+DMR_RawData_5 <- DMR_RawData_4 %>%
+  mutate(Concentration_3 = ifelse(
+    is.na(Concentration_3),
+    0.0,
+    ifelse(grepl("<|NODI B", Concentration_3), 0.0, Concentration_3)
+  ))
+
+Zeroes <- DMR_RawData_5 %>% filter(DMR_RawData_5$Concentration_3 == "0")
+
+# Concentration_2
+DMR_RawData_6 <- DMR_RawData_5 %>%
+  mutate(Concentration_2 = ifelse(
+    is.na(Concentration_2),
+    "0.0",
+    ifelse(grepl("<|NODI B", Concentration_2), "0.0", Concentration_2)
+  ))
+
+# Check the replacement worked
+Zeroes_2 <- DMR_RawData_6 %>% filter(DMR_RawData_6$Concentration_2 == "0" |
+                                       DMR_RawData_6$Concentration_3 == "0")
+
+# Standarize the format of zeroes
+DMR_RawData_6  <- DMR_RawData_6 %>%
+  mutate(Concentration_2 = case_when(
+    Concentration_2 %in% c("0", "0.00", "0.000", "0.0000", "0.0000000", "0.0000000") ~ "0.0",
+    TRUE ~ Concentration_2
+  ))
+
+# Standarize the format of zeroes
+DMR_RawData_7  <- DMR_RawData_6 %>%
+  mutate(Concentration_3 = case_when(
+    Concentration_3 %in% c("0", "0.00", "0.000", "0.0000", "0.0000000", "0.0000000") ~ "0.0",
+    TRUE ~ Concentration_3
+  ))
+
+# Check there arent any NA values
+NAValues <- DMR_RawData_7 %>% filter(is.na(Concentration_2) |
+                                       is.na(Concentration_3))
+
+# Convert Concentration_2 and # Concentration_3 to numeric
+DMR_RawData_7$Concentration_2 <- as.numeric(DMR_RawData_7$Concentration_2)
+DMR_RawData_7$Concentration_3 <- as.numeric(DMR_RawData_7$Concentration_3)
+
+
+# 4: Creating a results variable ####
+
+# Create Result Variable that is the average of Concentration_2 and Concentration_3 because these are duplicate samples taken on the same day.
+
+# Create a new variable for the average - when one of the concentrations is missing result should equal the value of the
+# concentration that isn't missing.
+DMR_RawData_7 <- DMR_RawData_7 %>%
+  mutate(Result = ifelse(
+    is.na(Concentration_2),
+    Concentration_3,
+    ifelse(
+      is.na(Concentration_3),
+      Concentration_2,
+      (Concentration_2 + Concentration_3) / 2
+    )
+  ))
+
+
+
+
+# 5: Subset the data to reduce # of columns ####
+
+
+DMR_Columns <- c(
+  "Facility_Name",
+  "NPDES_ID",
+  "Permit_Type",
+  "Permittee_Address",
+  "Latitude_in_Decimal_Degrees",
+  "Longitude_in_Decimal_Degrees",
+  "Mon._Period_End_Date",
+  "Concentration_Units",
+  "PFAS_Analyte",
+  "Result"
+)
+
+# Filter for the columns we need to retain
+DMR_RawData_8 <- DMR_RawData_7 %>%
+  select(all_of(DMR_Columns))
+
+
+# 6: Transpose from long to wide ####
+
+# Transpose the dataset long to wide
+
+# Filter data so there is one unique row for every combination of NPDES_ID, received date and parameter.
+DMR_Unique <- DMR_RawData_8 %>%
+  distinct(NPDES_ID,
+           Facility_Name,
+           Mon._Period_End_Date,
+           PFAS_Analyte,
+           .keep_all = TRUE)
+
+
+
+
+
+# Make sure result is numeric before transforming
+DMR_Unique$Result <- as.numeric(DMR_Unique$Result)
+
+
+
+# Round results column to 2 decimal place
+# Round the 'Value' column to 1 decimal place
+DMR_Unique$Result <- round(DMR_Unique$Result, digits = 1)
+
+
+# Transpose the data from long to wide format:
+# Id_Cols = specify the column(s) that you want to keep as identifiers (i.e., columns that remain as is and do not get spread)
+# names_from =  specifies the column from which the values will be spread to form new columns.
+# Values_from =  specifies the columns whose values will be spread into new columns (names_from columns).
+library(tidyr)
+library(dplyr)
+
+
+# First aggregate to ensure there is one unique row for every combination of NPDES_ID, Mon._Period_End_Date, and PFAS_Analyte.
+Aggregated_DMR <- DMR_Unique %>%
+  group_by(NPDES_ID, Mon._Period_End_Date, PFAS_Analyte) %>%
+  summarise(
+    across(
+      .cols = Result,
+      .fns = function(x)
+        ifelse(all(is.na(x)), NA, sum(x, na.rm = TRUE))
+    ),
+    across(.cols = everything(), .fns = ~ first(.))
+  ) %>%
+  ungroup()
+
+glimpse(Aggregated_DMR)
+
+
+
+
+# Transpose the aggregated data into wide format.
+
+library(tidyr)
+
+wide_data_DMR <- Aggregated_DMR %>%
+  pivot_wider(
+    id_cols = c(
+      "NPDES_ID",
+      "Permittee_Address",
+      "Facility_Name",
+      "Permit_Type",
+      "Latitude_in_Decimal_Degrees",
+      "Longitude_in_Decimal_Degrees",
+      "Mon._Period_End_Date",
+      "Concentration_Units"
+    ),
+    names_from = PFAS_Analyte,
+    values_from = Result,
+    values_fill = list(Result = 0)
+  )
+
+
+# 7: Rename and create new variables ####
+
+
+# Rename variables to standardize across datasets
+wide_data_DMR1 <- wide_data_DMR %>%
+  rename(
+    Site = Facility_Name,
+    `Site ID` = NPDES_ID,
+    Address = Permittee_Address,
+    `Sample date` = Mon._Period_End_Date,
+    Notes = Permit_Type,
+    Units = Concentration_Units,
+    Latitude = Latitude_in_Decimal_Degrees,
+    Longitude = Longitude_in_Decimal_Degrees
+  )
+
+
+# Create new variables
+
+wide_data_DMR2 <- wide_data_DMR1 %>% mutate(
+  Dataset = "Permits - Data Monitoring Reports",
+  Program = "CDPHE's Water Quality Control Division (WQCD) - Clean Water Program",
+  Link = "https://docs.google.com/document/d/1KyRl6b-t1o73jK7mlZ8mhHn8ubbBbgZL-2hk6A1PyaA/edit",
+  `Number of samples` = 2
+) |> mutate(Medium = ifelse(
+  Notes == "General Permit Covered Facility",
+  "Groundwater",
+  "Wastewater"
+)) |>
+  mutate(`Sum of PFOA and PFOS` = PFOA + PFOS) |>
+  mutate(`Sample ID` = paste0("DMR_", 1:nrow(wide_data_DMR2)))
+
+
+# 8: Assign variable types ####
+
+# Define the groups of columns based on your data dictionary
+char_cols <- c("Dataset",
+               "Program",
+               "Medium",
+               "Site",
+               "Site ID",
+               "Address",
+               "Link",
+               "Notes",
+               "Units")
+
+numeric_cols <- c("Latitude", "Longitude", "Number of samples")
+
+date_cols <- c("Sample date") # Note the date format is MM/DD/YYYY
+
+# Apply the transformations using mutate() and across()
+wide_data_DMR3 <- wide_data_DMR2 %>%
+  # 1. Convert Character Columns
+  mutate(across(.cols = all_of(char_cols), .fns = as.character)) %>%
+  
+  # 2. Convert Numeric Columns
+  mutate(across(.cols = all_of(numeric_cols), .fns = as.numeric)) %>%
+  
+  # 3. Convert Date Columns
+  mutate(across(
+    .cols = all_of(date_cols),
+    # Specify the format: %m = Month, %d = Day, %Y = 4-digit Year
+    .fns = ~ as.Date(., format = "%m/%d/%Y")
+  ))
+
+
+class(wide_data_DMR3$`Sample date`)
+
+
+# 9: Reorder variables ####
+
+# Create a character vector containing the EXACT column names in the desired order.
+desired_order <- c(
+  "Dataset",
+  "Program",
+  "Medium",
+  "Site",
+  "Site ID",
+  "Address",
+  "Latitude",
+  "Longitude",
+  "Link",
+  "Notes",
+  "Sample date",
+  "Sample ID",
+  "Number of samples",
+  "Units",
+  "Sum of PFOA and PFOS",
+  "PFOA",
+  "PFOS",
+  "PFHxS",
+  "PFNA",
+  "PFBS",
+  "GenX"
+)
+
+
+# Reorder variables using the select function
+wide_data_DMR4 <- wide_data_DMR3 |> select(all_of(desired_order), everything())
+
+# 10: Final misc. cleaning ####
+
+# Remove Total PFAS values from
+wide_data_DMR5  <- wide_data_DMR4  %>% select(-`Total PFOA and PFOS`)
+
+
+# Subset wastewater and groundwater
+UpdateDate_Permits_DMR_Groundwater <- wide_data_DMR5 %>% filter(Notes == "General Permit Covered Facility")
+UpdateDate_Permits_DMR_Wastewater <- wide_data_DMR5 %>% filter(Notes == "NPDES Individual Permit")
+
+# 11: Exporting ####
+
+# Export the data frame as a cleaned and formatted dataset.
+
+library("writexl")
+
+write_xlsx(
+  Update_DatePermits_DMR_Groundwater,
+  "X:\\Shared drives\\_CDPHE TEEO Data\\_Enviro\\PFAS\\PFAS Concentration Map May 2024 Update\\PFASMap 2024_RProject\\03_Clean_Data\\Groundwater\\DMR_GW_test_new.xlsx"
+)
+
+write_xlsx(
+  UpdateDate_Permits_DMR_Wastewater,
+  "X:\\Shared drives\\_CDPHE TEEO Data\\_Enviro\\PFAS\\PFAS Concentration Map May 2024 Update\\PFASMap 2024_RProject\\03_Clean_Data\\Wastewater\\DMR_Wastewater.xlsx"
+)
---
+++ 02_Scripts/Permits_Dewatering_Source_Water_GW.R
@@ -0,0 +1,265 @@
+# PERMITS - DEWATERING SOURCE WATER 
+
+
+# 1: Importing the data ####
+
+library(readxl)
+library(dplyr)
+library(tidyr)
+
+
+# Importing Data
+SourceWater_Raw <- read_excel("X:/Shared drives/_CDPHE TEEO Data/_Enviro/PFAS/PFAS Concentration Map May 2024 Update/PFASMap 2024_RProject/01_Raw_Data/Dewatering_SourceWater_May2024.xlsx")
+
+colnames(SourceWater_Raw)
+
+
+
+# 2: Misc Cleaning ####
+
+
+# List of columns to pivot
+columns_to_pivot <- c("COG318003", "COG318004", "COG318005", "COG318006", "COG318007", "COG318008", "COG318009", "COG318010", 
+                      "COG318011", "COG318012", "COG318013", "COG318014", "COG318015", "COG318016", "COG318017", "COG318018", 
+                      "COG318019", "COG318020", "COG318021", "COG318022", "COG318023", "COG318024", "COG318025", "COG318026", 
+                      "COG318027", "COG318029", "COG318032", "COG318036", "COG318037", "COG318060", "COG318094", "COG318095", 
+                      "COG318104", "COG318111", "COG318110", "COG318108", "COG318116", "COG318120", "COG318122", "COG318123", 
+                      "COG318124", "COG318119", "COG318125", "COG318112", "COG318126", "COG318127", "COG318128", "COG603005", 
+                      "COG603006", "COG603008", "COG603011", "COG603015", "COG603020", "COG603021", "COG603023", "COG603024", 
+                      "COG603032", "COG603033", "COG603036", "COG603065", "COG603078", "COG603081", "COG603087", "COG603090", 
+                      "COG603101", "COG603103", "COG603107", "COG603108", "COG603109", "COG603115", "COG603116", "COG603118", 
+                      "COG603123", "COG603125", "COG603127", "COG603145", "COG603148", "COG603149", "COG603150", "COG603151", 
+                      "COG603155", "COG603156", "COG603157", "COG603158", "COG603159", "COG603160", "COG603165", "COG603168", 
+                      "COG603174", "COG603178", "COG603179", "COG603183", "COG603190", "COG603199", "COG603205", "COG603218", 
+                      "COG603219", "COG603220", "COG603222", "COG603223", "COG603233", "COG603260", "COG603263", "COG603285", 
+                      "COG603293", "COG603294", "COG603295", "COG603331", "COG603333", "COG603352", "COG603356", "COG603359", 
+                      "COG603367", "COG603374", "COG603375", "COG603376", "COG603382", "COG603389", "COG603363", "COG603298", 
+                      "COG603189", "COG603128", "COG317058", "COG317061", "COG317062", "COG317072", "COG317075", 
+                      "COG317079", "COG317091", "COG317092", "COG317098", "COG317099", "COG317101", "COG080621", "COG317114...139", 
+                      "COG317114...140", "COG080689", "COG080690", "COG317109...143", "COG317109...144", "COG317118", "COG317120", 
+                      "COG317121", "COG080710", "COG317126...149","COG317126...150", "COG080830...151", "COG080830...152", "COG080830...153", 
+                      "COG080830...154", "COG080830...155", "COG080830...156", "COG080830...157", "COG317134", "COG080854", "COG317143", 
+                      "COG317144", "COG317149", "COG080968")
+
+# Ensure all specified columns are character so that we can pivot both the character and numeric data
+SourceWater_Raw[columns_to_pivot] <- lapply(SourceWater_Raw[columns_to_pivot], as.character)
+
+# Perform the pivot to make the different permit numbers all under one column "Permit Number"
+SourceLong <- pivot_longer(
+  SourceWater_Raw,
+  cols = all_of(columns_to_pivot),  # Using all_of to ensure the column names are correctly interpreted
+  names_to = "Permit_Number",
+  values_to = "Results"
+)
+
+# Filter for sample date and change sample_date variable to date format
+sample_dates <- SourceLong %>%
+  filter(Acronym == "Sample Date") %>%
+  select(Permit_Number, Sample_Date = Results)
+
+# Convert Excel date format to date format in R
+sample_dates$Sample_Date <- as.Date(as.numeric(sample_dates$Sample_Date), origin = "1899-12-30")
+
+# Filter for facility names
+Facility_Name <- SourceLong %>%
+  filter(Acronym == "Facility Name") %>%
+  select(Permit_Number, Facility_Name = Results) 
+
+# Filter for notes
+Notes <- SourceLong %>%
+  filter(Acronym == "Notes") %>%
+  select(Permit_Number, Notes = Results) 
+
+
+# Step 2: Remove rows where Acronym is "Sample Date", "Facility Name", or "Notes"
+SourceFiltered <- SourceLong %>%
+  filter(!(Acronym %in% c("Sample Date", "Facility Name", "Notes")))
+
+#  Join the fields you removed above
+SourceFinal <- SourceFiltered %>%
+  left_join(sample_dates, by = "Permit_Number") %>%
+  left_join(Facility_Name, by = "Permit_Number") %>%
+  left_join(Notes, by = "Permit_Number")
+
+
+SourceFinal <- SourceFinal %>% select(-Chemical_Name)
+
+
+# Replace NDs with 0
+SourceFinal <- SourceFinal %>%  
+  mutate(Results= ifelse(is.na(Results), "0.0",
+                         ifelse(grepl("<", Results), "0.0", Results)))
+
+# Transform the results column to numeric
+SourceFinal$Results <- as.numeric(SourceFinal$Results)
+class(SourceFinal$Results)
+
+# Round results column to 2 decimal place
+# Round the 'Value' column to 1 decimal place
+SourceFinal$Results  <- round(SourceFinal$Results , digits = 1)
+
+
+# Fix permit Numbers with...
+SourceLong1 <- SourceFinal %>%
+  mutate(Permit_Number = case_when(
+    Permit_Number %in% c("COG080830...151", "COG080830...152", "COG080830...153", "COG080830...154",
+                         "COG080830...155", "COG080830...156", "COG080830...157") ~ "COG080830",
+    Permit_Number %in% c("COG317109...143", "COG317109...144") ~ "COG317109",
+    Permit_Number %in% c("COG317114...139", "COG317114...140") ~ "COG317114",
+    Permit_Number  %in% c("COG317126...149", "COG317126...150") ~ "COG317126",
+    Permit_Number  %in% c("COG603023...124", "COG603023...58") ~ "COG603023",
+    TRUE ~ Permit_Number
+  ))
+
+
+# Aggregate the data before transforming it
+aggregated_data <- SourceLong1 %>%
+  group_by(Permit_Number, Sample_Date, Acronym, Quantification_Limit, Facility_Name, Notes) %>%
+  summarise(Sum_Results = sum(Results, na.rm = TRUE), .groups = "drop")
+
+
+
+
+Sourcewide1 <- aggregated_data %>%
+  pivot_wider(
+    id_cols = c("Permit_Number", "Sample_Date", "Facility_Name", "Notes"),
+    names_from = Acronym,
+    values_from = Sum_Results
+  ) %>%
+  rename_with(~ gsub("^Sum_Results_", "", .), starts_with("Sum_Results_"))
+
+
+
+# Update permit numbers using the notes column
+
+Sourcewide1 <- Sourcewide1 %>%
+  mutate(Permit_Number = case_when(
+    Permit_Number == "COG603128" ~ "COG318117",
+    Permit_Number == "COG603189" ~ "COG318115",
+    Permit_Number == "COG603023" ~ "COG318113",
+    Permit_Number == "COG603298" ~ "COG318112",
+    Permit_Number == "COG603363" ~ "COG318109",
+    TRUE ~ Permit_Number  # Keep the original value if no condition is met
+  ))
+
+# 3: Merging with lat/longs ####
+
+# Importing the file with lats and longs
+SourceWater_LatLongs <- read_excel("X:/Shared drives/_CDPHE TEEO Data/_Enviro/PFAS/PFAS Concentration Map May 2024 Update/PFASMap 2024_RProject/01_Raw_Data/Dewatering_SourceWater_LatLongs.xlsx")
+
+
+SourceWater_LatLongs_unique <- SourceWater_LatLongs %>%
+  group_by(Permit_Number, Latitude, Longitude) %>%
+  slice(1) %>%
+  ungroup()
+
+# Merge the lat longs dataset with the May 2024 dewatering source water data
+Sourcewide_LatsLongs <- left_join(Sourcewide1,
+                                  SourceWater_LatLongs_unique %>%
+                                    select(Permit_Number, Latitude, Longitude),
+                                  by = "Permit_Number")
+
+
+
+# Identify NA lat longs
+Sourcewide_LatsLongs_NA <- Sourcewide_LatsLongs %>% 
+  filter(is.na(Latitude))
+
+
+
+# 4: Rename and create new variables ####
+
+
+# Rename variables to standardize across datasets
+SourceWater1 <- Sourcewide_LatsLongs %>%
+  rename(
+    Site = Facility_Name,
+    `Site ID` = Permit_Number,
+    `Sample date` = Sample_Date
+  )
+
+
+# Create new variables and remove variables
+
+SourceWater2 <- SourceWater1 %>% mutate(
+  Dataset = "Permits - Dewatering Source Water Data",
+  Program = "CDPHE's Water Quality Control Division (WQCD) - Clean Water Program",
+  Link = "https://drive.google.com/file/d/1mZ6yS0ms4knYAQcimgKGptnThPt6cScM/view",
+  `Number of samples` = 1,
+  Medium = "Groundwater",
+  Units = "ng/L") |>
+  mutate(`Sum of PFOA and PFOS` = PFOA + PFOS) |>
+  mutate(`Sample ID` = paste0("Source_Water_", 1:nrow(SourceWater1))) 
+
+
+SourceWater3 <- SourceWater2 %>%  select(-c(Notes,`Division Calculated Sum of seven`, `ICIS Sum of seven`))
+
+
+# 5: Assign variable types ####
+
+# Define the groups of columns based on your data dictionary
+char_cols <- c("Dataset",
+               "Program",
+               "Medium",
+               "Site",
+               "Site ID",
+               "Link",
+               "Units")
+
+numeric_cols <- c("Latitude", "Longitude", "Number of samples")
+
+date_cols <- c("Sample date") # Note the date format is MM/DD/YYYY
+
+# Apply the transformations using mutate() and across()
+SourceWater4 <- SourceWater3 %>%
+  # 1. Convert Character Columns
+  mutate(across(.cols = all_of(char_cols), .fns = as.character)) %>%
+  
+  # 2. Convert Numeric Columns
+  mutate(across(.cols = all_of(numeric_cols), .fns = as.numeric)) %>%
+  
+  # 3. Convert Date Columns
+  mutate(across(
+    .cols = all_of(date_cols),
+    # Specify the format: %m = Month, %d = Day, %Y = 4-digit Year
+    .fns = ~ as.Date(., format = "%m/%d/%Y")
+  ))
+
+
+class(SourceWater4$`Sample date`)
+
+
+# 6: Reorder variables ####
+
+# Create a character vector containing the EXACT column names in the desired order.
+desired_order <- c(
+  "Dataset",
+  "Program",
+  "Medium",
+  "Site",
+  "Site ID",
+  "Latitude",
+  "Longitude",
+  "Link",
+  "Sample date",
+  "Sample ID",
+  "Number of samples",
+  "Units",
+  "Sum of PFOA and PFOS",
+  "PFOA",
+  "PFOS",
+  "PFHxS",
+  "PFNA",
+  "PFBS"
+)
+
+
+# Reorder variables using the select function
+UpdateDate_SourceWater_Clean_Groundwater<- SourceWater4 |> select(all_of(desired_order), everything())
+
+
+# 7: Export ####
+
+library("writexl")
+write_xlsx(UpdateDate_SourceWater_Clean_Groundwater,"X:\\Shared drives\\_CDPHE TEEO Data\\_Enviro\\PFAS\\PFAS Concentration Map May 2024 Update\\PFASMap 2024_RProject\\03_Clean_Data\\Groundwater\\SourceWater_Final_May24.xlsx")
+
